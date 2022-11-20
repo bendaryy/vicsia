33801bddf94803da4bc71074cd91ba880389c162
bendaryy <mohamedbendary1994@gmail.com>
2022-11-20 22:35:19 +0200

    vicsia

--- EInvoicing/SubmitInvoices2.bat
+++ EInvoicing/SubmitInvoices2.bat
@@ -1,2 +1,2 @@
-C:\laragon\www\vicsia\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\vicsia\EInvoicing 40489690 "Egypt Trust Sealing CA"
+C:\laragon\www\vicsia\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\vicsia\EInvoicing 19751818 "Egypt Trust Sealing CA"
 PAUSE
