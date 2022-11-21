bd3039a18bc60d24259de6dfcb892f46d73ae7fb
bendaryy <mohamedbendary1994@gmail.com>
2022-11-21 11:04:39 +0200

    ksljdf

--- app/Http/Controllers/manageDoucumentController.php
+++ app/Http/Controllers/manageDoucumentController.php
@@ -203,7 +203,7 @@ class manageDoucumentController extends Controller
         // End Bank payment
 
         $trnsformed = json_encode($invoice, JSON_UNESCAPED_UNICODE);
-        $myFileToJson = fopen("C:\laragon\www\halaltop\EInvoicing\SourceDocumentJson.json", "w") or die("unable to open file");
+        $myFileToJson = fopen("C:\laragon\www\vicsia\EInvoicing\SourceDocumentJson.json", "w") or die("unable to open file");
         fwrite($myFileToJson, $trnsformed);
         return redirect()->route('cer');
 
@@ -356,7 +356,7 @@ class manageDoucumentController extends Controller
         // End Bank payment
 
         $trnsformed = json_encode($invoice, JSON_UNESCAPED_UNICODE);
-        $myFileToJson = fopen("C:\laragon\www\halaltop\EInvoicing\SourceDocumentJson.json", "w") or die("unable to open file");
+        $myFileToJson = fopen("C:\laragon\www\vicsia\EInvoicing\SourceDocumentJson.json", "w") or die("unable to open file");
         fwrite($myFileToJson, $trnsformed);
         return redirect()->route('cer');
 
@@ -367,11 +367,11 @@ class manageDoucumentController extends Controller
     public function openBat()
     {
 
-        shell_exec('C:\laragon\www\halaltop\EInvoicing/SubmitInvoices2.bat');
-        $path = "C:\laragon\www\halaltop\EInvoicing/FullSignedDocument.json";
-        $path2 = "C:\laragon\www\halaltop\EInvoicing/Cades.txt";
-        $path3 = "C:\laragon\www\halaltop\EInvoicing/CanonicalString.txt";
-        $path4 = "C:\laragon\www\halaltop\EInvoicing/SourceDocumentJson.json";
+        shell_exec('C:\laragon\www\vicsia\EInvoicing/SubmitInvoices2.bat');
+        $path = "C:\laragon\www\vicsia\EInvoicing/FullSignedDocument.json";
+        $path2 = "C:\laragon\www\vicsia\EInvoicing/Cades.txt";
+        $path3 = "C:\laragon\www\vicsia\EInvoicing/CanonicalString.txt";
+        $path4 = "C:\laragon\www\vicsia\EInvoicing/SourceDocumentJson.json";
 
         $fullSignedFile = file_get_contents($path);
 
